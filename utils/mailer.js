06d522a6ee5c89b99ecda5168d5d480620cd2509
Pandula Mahesh <kariyapathirage@gmail.com>
2026-03-07 23:51:49 +0530

    Switch email service to Resend

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,29 +1,16 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
 
-console.log("MAIL_USER:", process.env.MAIL_USER);
-console.log("MAIL_PASS exists:", !!process.env.MAIL_PASS);
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  requireTLS: true,
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
-  },
-  family: 4,
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendMail({ to, subject, html }) {
-  return transporter.sendMail({
-    from: `"Apex Cabs" <${process.env.MAIL_USER}>`,
-    to,
-    subject,
-    html,
+  const response = await resend.emails.send({
+    from: `Apex Cabs <${process.env.MAIL_FROM}>`,
+    to: to,
+    subject: subject,
+    html: html,
   });
+
+  return response;
 }
 
 module.exports = { sendMail };
